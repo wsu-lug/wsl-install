b00f429a8d721586faddc1ed4119921cedb5d7fe
TheAwc24 <theawc24@gmail.com>
2022-09-14 15:51:58 -0700

    Changed Ubuntu version

--- install_ubuntu_wsl.cmd
+++ install_ubuntu_wsl.cmd
@@ -6,8 +6,8 @@ wsl --install
 Echo Making sure WSL is version 2
 wsl --set-default-version 2
 
-Echo Installing Ubuntu 22.04 LTS
-winget install "Ubuntu 22.04 LTS"
+Echo Installing Ubuntu 20.04 (22.04 install is not supported via command line)
+wsl --install -d Ubuntu
 
 Echo Starting a bash session, please run the next script in this session
 wsl
